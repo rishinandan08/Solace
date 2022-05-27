e1306ed6a9918c37057a5438371165e4631ac84c
rishinandan08 <65062233+rishinandan08@users.noreply.github.com>
2022-05-28 02:01:36 +0530

    Add files via upload

--- emotions.py
+++ emotions.py
@@ -16,7 +16,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 ap = argparse.ArgumentParser()
 ap.add_argument("--mode",help="train/display")
 mode = ap.parse_args().mode
-
+f=open("faceemo.txt", "w")
 # plots accuracy and loss curves
 def plot_model_history(model_history):
     """
@@ -87,6 +87,7 @@ model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
+
 # If you want to train the same model or try other models, go for this
 if mode == "train":
     model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
@@ -111,33 +112,51 @@ elif mode == "display":
 
     # start the webcam feed
     cap = cv2.VideoCapture(0)
+    i=0
     while True:
         # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
+        if i>50:
             break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        else:
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            # print(emotion_dict[maxindex])
-
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
             model1_result = []
-            model1_result.append(emotion_dict[maxindex])
-            print(model1_result)
 
-        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
+                f=open("faceemo.txt", "a")
+                f.write(emotion_dict[maxindex])
+                f.write(",")
+                f.close()
+                cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+                model1_result.append(emotion_dict[maxindex])
+                print(model1_result)
+                i=i+1
+
+            #cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+
+    f = open("faceemo.txt", "r")
+    f1 = (f.read()).split(',')
+    f1.pop(-1)
+
+    out = open("output1.txt", "w")
+    out.write(max(f1,key=f1.count))
+    out.close()
 
     cap.release()
     cv2.destroyAllWindows()
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+shell_exec('python emotions.py --mode display');
+?>
+<?php
+    // $myfilename = "output1.txt";
+    // if(file_exists($myfilename)){
+    //   echo file_get_contents($myfilename);
+    // }
+?>
